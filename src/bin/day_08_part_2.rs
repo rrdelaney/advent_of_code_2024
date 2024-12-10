84cedeb00a84694374028bda2d3ac55d66ef314f
Ryan Delaney <rrdelaney@outlook.com>
2024-12-10 00:26:14 -0800

    Day 08

---
+++ src/bin/day_08_part_2.rs
@@ -0,0 +1,94 @@
+use std::collections::HashSet;
+use std::io;
+use std::io::Read;
+
+fn main() -> io::Result<()> {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf)?;
+
+    let (nodes, bounds) = parse_map(&buf);
+    let count = count_antinodes(&nodes, bounds).len();
+    println!("{count}");
+
+    Ok(())
+}
+
+fn parse_map(map: &str) -> (Box<Vec<(char, i32, i32)>>, (i32, i32)) {
+    let mut nodes: Vec<(char, i32, i32)> = Vec::new();
+    let mut max_y = 0;
+    let mut max_x = 0;
+
+    for (y, line) in map.lines().enumerate() {
+        max_y = max_y.max(y);
+        for (x, char) in line.chars().enumerate() {
+            if y == 0 {
+                max_x = max_x.max(x);
+            }
+
+            if char == '.' {
+                continue;
+            }
+
+            nodes.push((char, y.try_into().unwrap(), x.try_into().unwrap()));
+        }
+    }
+
+    (
+        Box::from(nodes),
+        (max_y.try_into().unwrap(), max_x.try_into().unwrap()),
+    )
+}
+
+fn count_antinodes(
+    nodes: &Vec<(char, i32, i32)>,
+    (max_y, max_x): (i32, i32),
+) -> Box<HashSet<(i32, i32)>> {
+    let mut antinodes: HashSet<(i32, i32)> = HashSet::new();
+
+    for (char, y, x) in nodes {
+        for (anti_char, anti_y, anti_x) in nodes.iter() {
+            if char != anti_char {
+                continue;
+            } else if x == anti_x && y == anti_y {
+                continue;
+            }
+
+            let diff_y = y - anti_y;
+            let diff_x = x - anti_x;
+            let mut antinode_y = *y;
+            let mut antinode_x = *x;
+            while 0 <= antinode_y && antinode_y <= max_y && 0 <= antinode_x && antinode_x <= max_x {
+                antinodes.insert((antinode_y, antinode_x));
+                antinode_y += diff_y;
+                antinode_x += diff_x;
+            }
+        }
+    }
+
+    Box::from(antinodes)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum_invalid_middle_pages() {
+        let test_data = r"............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............";
+
+        let (nodes, bounds) = parse_map(test_data);
+        let antinodes = count_antinodes(&nodes, bounds);
+        assert_eq!(antinodes.len(), 34);
+    }
+}
